c391fae93f9b41c24d2fac5cba1767388eb9fc16
suniaikin <class.suniaikin@gmail.com>
2025-07-28 13:31:26 -0700

    strings

--- 2.js
+++ 2.js
@@ -1,3 +1,4 @@
+/*
 // seconds in an hour
 let secondInAMinute = 60
 let minutesInAnHour = 60
@@ -18,8 +19,85 @@ console.log(secondsInAYear)
 let age = 35
 let ageInSeconds = age*secondsInAYear
 console.log(ageInSeconds)
-
+*/
 // Incrementing And Decrementing //
-let
+/*let highFives = 0
+++highFives
+console.log(highFives)
+++highFives
+console.log(highFives)
+--highFives
+console.log(highFives)
+let highFives = 0
+highFives++
+highFives++
+highFives++
+console.log(highFives)
+
+highFives = 0
+++highFives
+++highFives
+++highFives
+console.log(highFives)
+let x = 10
+x = x + 5
+console.log(x)
+x += 5
+console.log(x)
+x -=5
+console.log(x)
+let balloons = 100
+balloons *= 4
+console.log(balloons)*/
+
+// Strings
+/*let myAwesomeString = "Hello World";
+myAwesomeString = 5
+console.log(myAwesomeString);
+let numberNine = 9
+let stringNine = "9"
+let sum = numberNine  stringNine;
+console.log(sum);
+let greeting = "Hello "
+let myNickname = "Nick"
+console.log(greeting + myNickname)*/
+
+//Finding the Length of a String
+/*let stringLength = "Supercalifragilisticexpialidocious"
+console.log(stringLength.length);
+let javascript = "java" + "script";
+console.log(javascript.length);*/
+
+// Getting a Single Character from a String
+/*let myName = "Andrew Rivers"
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName[2]);*/
+
+// Cutting Up Strings
+/*
+
+let longString = "My long string is long";
+let slicedWords = longString.slice(3, 14)
+console.log(slicedWords);
+
+let slicedWord = longString.slice(3)
+console.log(slicedWord);
+*/
+
+// Changing Strings to All Capital
+/*let firstUpperCase = "Hello there, how are you doing?"
+let upperCase = firstUpperCase.toUpperCase()
+console.log(upperCase) // HELLO THERE, HOW ARE YOU DOING?
+
+firstLowerCase = "hELlo THERE, hOW ARE yOu doINg?"
+let lowerCase = firstLowerCase.toLowerCase()
+console.log(lowerCase) // hello there, how are you doing?*/
+
+// TASK: how to turn the firstLowerCase into firstUpperCase ==//
+let firstLowerCase = "hELlo THERE, hOW ARE yOu doINg?"
+let upperCase = firstLowerCase.toUpperCase()
+
+console.log(upperCase)
 
 
