57a225f83a46de99900075dadacae5b0ca23c397
suniaikin <class.suniaikin@gmail.com>
2025-08-02 16:36:51 -0700

    decision maker

--- Part I Fundamentals/6_Useful Things to Do with Arrays/6.2_Decision Maker.js
+++ Part I Fundamentals/6_Useful Things to Do with Arrays/6.2_Decision Maker.js
@@ -1,18 +1,33 @@
 // Using Math.random()
+
 let random = Math.random()
 console.log(random)
-
 random = Math.random()*10
 console.log(random)
 
 // Rounding Down with Math.floor()
+
 let roudedRandom = Math.floor(random)
 console.log(roudedRandom)
-
 let randomIndex = Math.floor(Math.random() * 4);
 console.log(randomIndex)
 
 // Random Element from the array
-let array = ["Rock", "Paper", "Scissors"]
+
+let array = ["Rock", "Paper", "Scissors", "Explosion", "Cave", "Princess", "Pen"]
 let randomChoice = array[Math.floor(Math.random()*array.length)]
 console.log(randomChoice)
+
+// The Complete Decision Maker
+
+let phrases = [
+"That sounds good",
+"Yes, you should definitely do that",
+"I'm not sure that's a great idea",
+"Maybe not today?",
+"Computer says no."
+];
+
+let randomAnswer = phrases[Math.floor( Math.random() * phrases.length)]
+console.log(randomAnswer)
+
---
+++ Part I Fundamentals/6_Useful Things to Do with Arrays/6.3_Creating a Random Generator.js
@@ -0,0 +1,10 @@
+let randomBodyParts = ["Face", "Nose", "Hair"];
+let randomAdjectives = ["Smelly", "Boring", "Stupid"];
+let randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
+
+let randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)]
+let randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)]
+let randomWord = randomWords[Math.floor(Math.random() * randomWords.length)]
+
+let jointSentence = "Your " + randomBodyPart + "is like a " + randomAdjective + " " + randomWord + "!";
+console.log(jointSentence)
