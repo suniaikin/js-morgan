fa5d36d3e9f59f2fb7d2e612fa6a392d483402bd
suniaikin <class.suniaikin@gmail.com>
2025-08-02 14:08:49 -0700

    finding your way

---
+++ Part I Fundamentals/6_Useful Things to Do with Arrays/6.1_Finding Your Way Home.js
@@ -0,0 +1,40 @@
+// Building the Array with push
+
+let landmark = []
+landmark.push('My house')
+console.log(landmark)
+landmark.push('Front path')
+console.log(landmark)
+landmark.push('Flickering streetlamp')
+console.log(landmark)
+landmark.push('Fire station')
+console.log(landmark)
+landmark.push('My friend\'s house')
+console.log(landmark)
+// Way back to home
+
+let backStep_1 = landmark[landmark.length - 1]
+console.log("-", backStep_1)
+landmark.pop()
+console.log(landmark)
+
+let backStep_2 = landmark[landmark.length - 1]
+console.log("-", backStep_2)
+landmark.pop()
+console.log(landmark)
+
+let backStep_3 = landmark[landmark.length - 1]
+console.log("-", backStep_3)
+landmark.pop()
+console.log(landmark)
+
+let backStep_4 = landmark[landmark.length - 1]
+console.log("-", backStep_4)
+landmark.pop()
+console.log(landmark)
+
+let backStep_5 = landmark[landmark.length - 1]
+console.log("-", backStep_5)
+landmark.pop()
+console.log(landmark)
+
