9f9143c0e57c0889fc4ace2491185732d3f14169
suniaikin <class.suniaikin@gmail.com>
2025-08-03 11:52:05 -0700

    task 3.3

---
+++ tasks/3.3_Joining Numbers.js
@@ -0,0 +1,12 @@
+// 🎃DESCRIPTION🎃
+/* How could you turn the array [3, 2, 1] into the string
+"3 is bigger than 2 is bigger than 1" using the join method? */
+
+// ✅SOLUTION✅
+
+const arr = [3, 2, 1]
+const newString = arr.join(" is bigger than ")
+console.log(newString) // 3 is bigger than 2 is bigger than 1
+
+// 💖BEST PRACTICE💖
+
