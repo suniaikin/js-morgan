ed4bfbc5ec140ad535d5113f95f170b6a106d73e
suniaikin <class.suniaikin@gmail.com>
2025-08-03 23:43:34 -0700

    array of friends

--- Part I Fundamentals/7_Objects/7.4_Combining Arrays and Objects.js
+++ Part I Fundamentals/7_Objects/7.4_Combining Arrays and Objects.js
@@ -12,3 +12,10 @@ let dinosaurs = [
 		period: "Triassic"
 	}
 ]
+
+let firstElement = dinosaurs[0]
+console.log(firstElement) // { name: 'Tyrannosaurus Rex', period: 'Late Cretaceous' }
+let getOnlyName = dinosaurs[0].name // Tyrannosaurus Rex
+let getOnlyName2 = dinosaurs[0]["name"]
+console.log(getOnlyName) // Tyrannosaurus Rex
+console.log(getOnlyName2) // Tyrannosaurus Rex
---
+++ Part I Fundamentals/7_Objects/7.5_An Array of Friends.js
@@ -0,0 +1,15 @@
+let anna = {
+	name: "Anna",
+	age: 11,
+	luckyNumbers: [2, 4, 8, 16]
+};
+let dave = {
+	name: "Dave",
+	age: 5,
+	luckyNumbers: [3, 9, 40]
+};
+let kate = {
+	name: "Kate",
+	age: 9,
+	luckyNumbers: [1, 2, 3]
+};
