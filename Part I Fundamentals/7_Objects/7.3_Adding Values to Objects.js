8491a70ae0de6822a962ff57fc2318d5b65948c2
suniaikin <class.suniaikin@gmail.com>
2025-08-03 23:33:28 -0700

    adding keys

--- Part I Fundamentals/7_Objects/7.3_Adding Values to Objects.js
+++ Part I Fundamentals/7_Objects/7.3_Adding Values to Objects.js
@@ -5,4 +5,15 @@ cat["color"] = "Tortoiseshell"
 console.log(cat); // { legs: 3, name: 'Harmony', color: 'Tortoiseshell' }
 
 // Adding Keys with Dot Notation
+let cat2 = {}
+cat2.legs = 3;
+cat2.name = "Harmony";
+cat2.color = "Tortoiseshell"
+console.log(cat2);
+
+let dog = {
+name: "Pancake",
+legs: 4,
+isAwesome: true
+}
 
