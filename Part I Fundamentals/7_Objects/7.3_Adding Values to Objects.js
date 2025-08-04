e346b434b064a7daf1069f88c79a3cfafff41043
suniaikin <class.suniaikin@gmail.com>
2025-08-03 21:15:34 -0700

    ading wit dot

---
+++ Part I Fundamentals/7_Objects/7.3_Adding Values to Objects.js
@@ -0,0 +1,8 @@
+let cat = {}; // Empty object
+cat["legs"] = 3;
+cat["name"] = "Harmony";
+cat["color"] = "Tortoiseshell"
+console.log(cat); // { legs: 3, name: 'Harmony', color: 'Tortoiseshell' }
+
+// Adding Keys with Dot Notation
+
