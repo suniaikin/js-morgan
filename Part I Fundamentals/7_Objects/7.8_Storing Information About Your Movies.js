6d68b0e338a109054b00c90b16319e14a114d86c
suniaikin <class.suniaikin@gmail.com>
2025-08-04 11:36:08 -0700

    storing movies

---
+++ Part I Fundamentals/7_Objects/7.8_Storing Information About Your Movies.js
@@ -0,0 +1,36 @@
+let movies = {
+	"Finding Nemo": {
+		releaseDate: 2003,
+		duration: 100,
+		actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+		format: "DVD"
+	},
+	"Star Wars: Episode VI - Return of the Jedi": {
+		releaseDate: 1983,
+		duration: 134,
+		actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+		format: "DVD"
+	},
+	"Harry Potter and the Goblet of Fire": {
+		releaseDate: 2005,
+		duration: 157,
+		actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+		format: "Blu-ray"
+	}
+}
+
+const findingNemo = movies["Finding Nemo"];
+
+console.log(findingNemo.format)
+
+let cars = {
+	releaseDate: 2006,
+	duration: 117,
+	actors: ["Owen Wilson", "Bonnie Hunt", "Paul Newman"],
+	format: "Blu-ray"
+}
+
+movies["Cars"] = cars;
+
+console.log(movies)
+console.log(Object.keys(movies))
