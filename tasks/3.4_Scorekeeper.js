27e80d561b029637beaef5e3eec3881383fe2709
suniaikin <class.suniaikin@gmail.com>
2025-08-04 11:54:49 -0700

    task 3.4

---
+++ tasks/3.4_Scorekeeper.js
@@ -0,0 +1,24 @@
+// 🎃DESCRIPTION🎃
+/* Imagine you’re playing a game with some friends and you
+want to keep track of the score. Create an object called
+scores. The keys will be the names of your friends, and the
+values will be the scores (which will all start at 0). As the
+players earn points, you must increase their scores. How
+would you increase a player’s score in the scores object? */
+
+// ✅SOLUTION✅
+
+let scores = {}
+scores["Jimmy"] = 0
+scores["Jane"] = 0
+scores["John"] = 0
+
+scores["Jimmy"] += 2
+scores["Jane"] += 3
+scores["John"] += 4
+
+console.log(scores)
+
+
+// 💖BEST PRACTICE💖
+
