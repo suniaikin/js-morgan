662ae81e1743c194be06f0b90781fa930c37b83a
suniaikin <class.suniaikin@gmail.com>
2025-08-06 00:48:19 -0700

    power of 3 by While

---
+++ tasks/10_Power of 3 WHILE.js
@@ -0,0 +1,12 @@
+// 🎃DESCRIPTION🎃
+/*Write a loop to print the powers of 3 under 10,000 (it should print 3, 9, 27, etc.).
+Rewrite this loop with a while loop.   */
+
+// ✅SOLUTION✅
+let i = 3
+while (i < 10000) {
+	console.log(i = i * 3);
+}
+
+// 💖BEST PRACTICE💖
+
