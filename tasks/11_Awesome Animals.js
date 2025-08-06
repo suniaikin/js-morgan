837a9c1de9a2f78f254bd70c237a2328edb5ab6a
suniaikin <class.suniaikin@gmail.com>
2025-08-06 09:31:22 -0700

    awesome animals

---
+++ tasks/11_Awesome Animals.js
@@ -0,0 +1,15 @@
+// 🎃DESCRIPTION🎃
+/*Write a for loop that modifies an array of animals, making them
+awesome!  */
+
+// ✅SOLUTION✅
+const animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"]
+for(let i = 0; i<animals.length;i++) {
+	console.log(`Awesome ${animals[i]}`)
+}
+
+// 💖BEST PRACTICE💖
+
+
+
+
