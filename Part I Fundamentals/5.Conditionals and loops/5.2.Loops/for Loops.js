4d493d684619e807d8f88cbdba948e379ce843f9
suniaikin <class.suniaikin@gmail.com>
2025-08-05 22:18:23 -0700

    for Loops

---
+++ Part I Fundamentals/5.Conditionals and loops/5.2.Loops/for Loops.js
@@ -0,0 +1,10 @@
+/*
+for loops make it easier to write loops that create a variable, loop
+until a condition is true, and update the variable at the end of
+each turn around the loop.
+ */
+
+for (let sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+	console.log(`I have counted ${sheepCounted} sheep!`)
+}
+console.log("Zzzzzzz")
