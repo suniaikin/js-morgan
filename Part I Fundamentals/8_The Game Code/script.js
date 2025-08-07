4ef5ce2149925357ddb7fb5c1df95914a023102d
suniaikin <class.suniaikin@gmail.com>
2025-08-07 15:11:01 -0700

    game loop

--- Part I Fundamentals/8_The Game Code/script.js
+++ Part I Fundamentals/8_The Game Code/script.js
@@ -12,7 +12,13 @@ console.log(word);
 
 // Set up the answer array
 let answerArray = [];
-for(let i = 0; i < words.length; i++) {
+for (let i = 0; i < words.length; i++) {
 	answerArray[i] = "_";
 }
 let remainingLetters = words.length;
+
+// The game loop
+while (remainingLetters > 0) {
+
+	
+}
