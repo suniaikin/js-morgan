f50f40097a2258942698a2c042aa302a59346042
suniaikin <class.suniaikin@gmail.com>
2025-08-07 14:12:24 -0700

    coding the game

--- Part I Fundamentals/7_Creating a Hangman Game/7_3_Coding the Game/script.js
+++ Part I Fundamentals/7_Creating a Hangman Game/7_3_Coding the Game/script.js
@@ -1,17 +1,49 @@
-// Choosing a Random Word
+//=== Choosing a Random Word
+
 const words = [
 	"javascript",
 	"monkey",
 	"amazing",
 	"pancake"
 ]
-const word = words[Math.floor(Math.random()*words.length)]
+//=== Function of choosing a random word
 
-// Creating the Answer Array
+const word = words[Math.floor(Math.random() * words.length)]
+
+//=== Creating the Answer Array
+
 let answerArray = []
 for (let i = 0; i < word.length; i++) {
 	answerArray[i] = "_";
 }
+console.log(answerArray);
+
+//=== How many letters are left to be guessed
 
 let remainingLetters = word.length
-console.log(answerArray)
+
+/* Every time the player guesses a correct letter, this value will be decremented (reduced) by 1 for each instance of that letter in the word.*/
+
+
+//=== Showing the Player’s Progress
+
+alert(answerArray.join(" "));
+
+//=== Handling the Player’s Input
+
+let guess = prompt("Guess a letter, or click Cancel to stop playing.");
+
+while (remainingLetters > 0) {
+	if (guess === null) {
+		break
+	} else if (guess.length !== 1) {
+		alert("Please enter a single letter!");
+	} else {
+		for (let j = 0; j < word.length; j++) {
+			if (word[j] === guess) {
+				answerArray[j] = guess;
+				remainingLetters--
+			}
+		}
+	}
+}
