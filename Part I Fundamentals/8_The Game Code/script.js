1431cb1008d6975afbe931515c0f8cdcf2a1edc1
suniaikin <class.suniaikin@gmail.com>
2025-08-07 15:14:20 -0700

    get an answer

--- Part I Fundamentals/8_The Game Code/script.js
+++ Part I Fundamentals/8_The Game Code/script.js
@@ -19,10 +19,17 @@ let remainingLetters = words.length;
 
 // The game loop
 while (remainingLetters > 0) {
-     // Show the player their progress
+	// Show the player their progress
 	alert(answerArray.join(" "));
+	// Get a guess from the player
+	let guess = prompt("Guess a letter, or click Cancel to stop playing")
+	if (guess === null) {
+		// Exit the game loop
+		break;
+	} else if (guess.length !== 1) {
+		alert("Please enter a single letter.");
+	} else {
 	
-
-}
+	}
 	
 }
