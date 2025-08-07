8ddf50971d4623593e27d668409cbd37d6fe8b8b
suniaikin <class.suniaikin@gmail.com>
2025-08-07 15:28:55 -0700

    final

--- Part I Fundamentals/8_The Game Code/script.js
+++ Part I Fundamentals/8_The Game Code/script.js
@@ -5,18 +5,14 @@ let words = [
 	"amazing",
 	"pancake"
 ];
-
 // Pick a random word
 let word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
-
 // Set up the answer array
 let answerArray = [];
 for (let i = 0; i < words.length; i++) {
 	answerArray[i] = "_";
 }
 let remainingLetters = words.length;
-
 // The game loop
 while (remainingLetters > 0) {
 	// Show the player their progress
@@ -29,7 +25,15 @@ while (remainingLetters > 0) {
 	} else if (guess.length !== 1) {
 		alert("Please enter a single letter.");
 	} else {
-	
+		// Update the game state with the guess
+		for (let j = 0; j < word.length; j++) {
+			if (word[j] === guess) {
+				answerArray[j] = guess;
+				remainingLetters--;
+			}
+		}
 	}
 	
 }
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
