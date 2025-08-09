bf0bb6240bdd47be3de05e60b62cef796cf4799a
suniaikin <class.suniaikin@gmail.com>
2025-08-09 00:32:41 -0700

    final

--- Part I Fundamentals/8_The Game Code/script.js
+++ Part I Fundamentals/8_The Game Code/script.js
@@ -12,34 +12,48 @@ let words = [
 let word = words[Math.floor(Math.random() * words.length)];
 // Set up the answer array
 let answerArray = [];
-for (let i = 0; i < words.length; i++) {
+for (let i = 0; i < word.length; i++) {
 	answerArray[i] = "_";
 }
-let remainingLetters = words.length;
+let remainingLetters = word.length;
+
 
 // Attempts
 let attempts = 0;
 let MAX_ATTEMPTS = 3;
 
 // The game loop
-while (remainingLetters > 0 && attempts < 6) {
+while (remainingLetters > 0 && attempts < MAX_ATTEMPTS) {
 	// Show the player their progress
 	alert(answerArray.join(" "));
 	// Get a guess from the player
-	let guess = prompt("Guess a letter, or click Cancel to stop playing").toLowerCase();
+	let guess  = prompt("Guess a letter, or click Cancel to stop playing");
 	if (guess === null) {
 		// Exit the game loop
 		break;
-	} else if (guess.length !== 1) {
+	}
+	guess = guess.toLowerCase();
+	if (guess.length !== 1) {
 		alert("Please enter a single letter.");
+		continue;
 	} else {
 		// Update the game state with the guess
+		let alreadyGuessed = false;
+		for(let j = 0; j < answerArray.length; j++) {
+			if (answerArray[j] === guess) {
+				alreadyGuessed = true;
+				break;
+			}
+		}
+		if (alreadyGuessed) {
+			alert("You already guessed that letter.");
+			continue;
+		}
 		let foundLetter = false;
-		let usedLetter = false;
-		for (let j = 0; j < word.length; j++) {
-			if (word[j] === guess && answerArray[j] === "_") {
-				answerArray[j] = guess;
-				if (answerArray[j] !== "_" ) {
+		for (let x = 0; x < word.length; x++) {
+			if (word[x] === guess && answerArray[x] === "_") {
+				answerArray[x] = guess;
+				if (answerArray[x] !== "_" ) {
 					remainingLetters--;
 					foundLetter = true;
 				}
