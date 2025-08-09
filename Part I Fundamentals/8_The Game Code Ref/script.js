0914384c5c93d2ab1d0afb26100ad36ebb310852
suniaikin <class.suniaikin@gmail.com>
2025-08-09 11:07:32 -0700

    ref

--- Part I Fundamentals/8_The Game Code Ref/script.js
+++ Part I Fundamentals/8_The Game Code Ref/script.js
@@ -8,39 +8,67 @@ let words = [
 	"programming",
 	"computer"
 ];
+
+
 // Pick a random word
 let word = words[Math.floor(Math.random() * words.length)];
+
+
 // Set up the answer array
 let answerArray = [];
+
+
+// Change the word's letters to _
 for (let i = 0; i < word.length; i++) {
 	answerArray[i] = "_";
 }
+
+// Set up a variable for calculation of the rest letters
 let remainingLetters = word.length;
 
 
-// Attempts
+// Set up a variable to calculate attempts
 let attempts = 0;
-let MAX_ATTEMPTS = 3;
+let MAX_ATTEMPTS = 6;
 
-// The game loop
+
+
+// THE GAME LOOP
 while (remainingLetters > 0 && attempts < MAX_ATTEMPTS) {
-	// Show the player their progress
+	
+	// 1st Step: Show the player their progress - transform their answers Array to a String
 	alert(answerArray.join(" "));
-	// Get a guess from the player
+	
+	// 2nd Step: Get a guess from the player
 	let guess  = prompt("Guess a letter, or click Cancel to stop playing");
+	
+	// Checking if the user input any symbol
 	if (guess === null) {
-		// Exit the game loop
+		
+		// If no symbol input, exit the game loop
 		break;
 	}
+	
+	// If user input a Capital letter
 	guess = guess.toLowerCase();
+	
+	// If user input more than one letter
 	if (guess.length !== 1) {
 		alert("Please enter a single letter.");
 		continue;
+	
+	// If user input one letter
 	} else {
-		// Update the game state with the guess
+		
+		// Set up a variable to check if the Letter was already input
 		let alreadyGuessed = false;
+		
+		// Set up a loop to run through the String and check if the input Letter is already there
 		for(let j = 0; j < answerArray.length; j++) {
+			
+			// If the input Letter equals the symbol saved in answerArray...
 			if (answerArray[j] === guess) {
+				// ...we state that alreadyGuessed is true and stop the loop
 				alreadyGuessed = true;
 				break;
 			}
