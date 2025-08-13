75050ef4773eeffb8b16f26d9f99e9f955bf7c50
suniaikin <class.suniaikin@gmail.com>
2025-08-12 23:26:31 -0700

    function

--- Part I/8_Functions/leaving.js
+++ Part I/8_Functions/leaving.js
@@ -1,9 +1,26 @@
-/
+/* As soon as the JavaScript interpreter reaches return in a function, 
+it leaves the function, even if more code remains in the
+function body */
 
 let fifthLetter = function (name) {
-    if (name.length < 5) {
+    if (name.length < 6  ) {
     return;
     }
-    return "Пятая буква вашего имени: " + name[4] + ".";
+    return "Пятая буква вашего имени: " + name[1] + ".";
     }
-	console.log(fifthLetter('Andr'));
+
+    console.log(fifthLetter("Andrei"))
+
+/* Using return Multiple Times Instead of if...else Statements */
+
+let medalForScore = function (score) {
+    if (score < 3) {
+    return "Bronze";
+    }
+    if (score < 7) {
+    return "Silver";
+    }
+    return "Gold";
+    }
+
+    console.log(medalForScore(5))
