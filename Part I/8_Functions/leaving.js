e1d9f7b309d6ada2c81f893acffe2c0ebd3ad194
suniaikin <class.suniaikin@gmail.com>
2025-08-12 23:18:34 -0700

    function

--- Part I/8_Functions/leaving.js
+++ Part I/8_Functions/leaving.js
@@ -1,6 +1,9 @@
+/
+
 let fifthLetter = function (name) {
     if (name.length < 5) {
     return;
     }
     return "Пятая буква вашего имени: " + name[4] + ".";
     }
+	console.log(fifthLetter('Andr'));
