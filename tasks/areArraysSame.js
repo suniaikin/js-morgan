6fb41735f4cfdf10c357d7dfaead3e5b2e024ede
suniaikin <class.suniaikin@gmail.com>
2025-08-13 21:31:22 -0700

    task3

---
+++ tasks/areArraysSame.js
@@ -0,0 +1,23 @@
+function areArraysSame (arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(areArraysSame([1, 2, 3], [1, 2, 3]))
+console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4]))
+console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4]))
+
+
+
+
+
+
+
+
