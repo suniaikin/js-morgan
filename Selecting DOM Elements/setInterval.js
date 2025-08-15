73f172a3777f3ad1b9e7d50a39a572a7b621201d
suniaikin <class.suniaikin@gmail.com>
2025-08-14 22:05:48 -0700

    interval

---
+++ Selecting DOM Elements/setInterval.js
@@ -0,0 +1,11 @@
+let counter = 1;
+let printMessage = function () {
+console.log("Ты смотришь в консоль уже " + counter + " сек");
+counter++;
+};
+if (counter > 10) {
+    clearInterval(intervalId);
+    console.log("Интервал остановлен!");
+}
+let intervalId = setInterval(printMessage, 1000);
+console.log(intervalId)
